7be5f6abd707493b0796b51084793fce27a8fe45
Joseph <sjoseph1@umbc.edu>
2016-07-25 14:52:25 -0400

    Added Assignment 2

---
+++ com/week2/day1/assignment2/Assignment2.java
@@ -0,0 +1,117 @@
+package com.week2.day1.assignment1;
+
+import java.util.Date;
+
+public class Assignment2
+{
+    private float sizeOfDisk;
+    private float numAcresInPlot;
+    private String nameFile;
+    private String phoneNum;
+    private String companyName;
+    private Date dateToday;  
+    private boolean isLightSwitchOn;
+    private double pi; 
+    private double speedOfLight;
+    private Vehicle myCar;
+    private Address myAddress;
+    private Product[] myProducts;
+    
+    public float getSizeOfDisk()
+    {
+        return sizeOfDisk;
+    }
+    public void setSizeOfDisk(float sizeOfDisk)
+    {
+        this.sizeOfDisk = sizeOfDisk;
+    }
+    public float getNumAcresInPlot()
+    {
+        return numAcresInPlot;
+    }
+    public void setNumAcresInPlot(float numAcresInPlot)
+    {
+        this.numAcresInPlot = numAcresInPlot;
+    }
+    public String getNameFile()
+    {
+        return nameFile;
+    }
+    public void setNameFile(String nameFile)
+    {
+        this.nameFile = nameFile;
+    }
+    public String getPhoneNum()
+    {
+        return phoneNum;
+    }
+    public void setPhoneNum(String phoneNum)
+    {
+        this.phoneNum = phoneNum;
+    }
+    public String getCompanyName()
+    {
+        return companyName;
+    }
+    public void setCompanyName(String companyName)
+    {
+        this.companyName = companyName;
+    }
+    public Date getDateToday()
+    {
+        return dateToday;
+    }
+    public void setDateToday(Date dateToday)
+    {
+        this.dateToday = dateToday;
+    }
+    public boolean isLightSwitchOn()
+    {
+        return isLightSwitchOn;
+    }
+    public void setLightSwitchOn(boolean isLightSwitchOn)
+    {
+        this.isLightSwitchOn = isLightSwitchOn;
+    }
+    public double getPi()
+    {
+        return pi;
+    }
+    public void setPi(double pi)
+    {
+        this.pi = pi;
+    }
+    public double getSpeedOfLight()
+    {
+        return speedOfLight;
+    }
+    public void setSpeedOfLight(double speedOfLight)
+    {
+        this.speedOfLight = speedOfLight;
+    }
+    public Vehicle getMyCar()
+    {
+        return myCar;
+    }
+    public void setMyCar(Vehicle myCar)
+    {
+        this.myCar = myCar;
+    }
+    public Address getMyAddress()
+    {
+        return myAddress;
+    }
+    public void setMyAddress(Address myAddress)
+    {
+        this.myAddress = myAddress;
+    }
+    public Product[] getMyProducts()
+    {
+        return myProducts;
+    }
+    public void setMyProducts(Product[] myProducts)
+    {
+        this.myProducts = myProducts;
+    }
+    
+}
